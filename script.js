e787a886e9bc25a04284c2584a8cf8b3dadd1cba
ShirtHanger <52720578+ShirtHanger@users.noreply.github.com>
2024-09-10 16:20:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Welcome!")
+$("button").on("click",
+  function()
+    {alert("Thank you for reading!")});
+$("h2").on("click",
+  function()
+    {alert("You can read more on this at kujo-sakana.jp")});
+$("img").on("click",
+  function()
+    {alert("Source for copyrighted images can be found below!")});
+$("p").on("click",
+  function()
+    {alert("References can be found at the bottom of the page!")});
+
+console.log("It's alive!")
